5da74b6602a386e5595d79980c2ced576fb36775
Renbing Zhang <1693625254@qq.com>
2020-01-14 23:36:41 +0800

    Add files via upload

---
+++ ftp.py
@@ -0,0 +1,9 @@
+from ftplib import FTP
+def main():
+    ftp=FTP('ftp.ibilio.org')
+    print("Welcome:",ftp.getwelcome())
+    ftp.login()
+    print("Current working directory:",ftp.pwd())
+    ftp.quit()
+if __name__ == '__main__':
+    main()
